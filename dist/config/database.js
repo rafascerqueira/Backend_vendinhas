c527f10fe3587fc1106a15e25d36fab960e58740
Rafael Cerqueira <rafascerqueira.dev@gmail.com>
2019-11-10 22:27:10 -0300

    Change dist files

--- dist/config/database.js
+++ dist/config/database.js
@@ -1,6 +1,7 @@
 "use strict";
 
 // require("dotenv").config();
+// change src/database/postgres.js to use
 module.exports = {
   dialect: "postgres",
   host: process.env.DATABASE_URL,
@@ -10,6 +11,9 @@ module.exports = {
   define: {
     timestamps: true,
     underscored: true
+  },
+  production: {
+    use_env_variable: "DATABASE_URL"
   }
 };
 //# sourceMappingURL=database.js.map
--- dist/database/postgres.js
+++ dist/database/postgres.js
@@ -1,12 +1,17 @@
 "use strict";
 
-const Sequelize = require("sequelize");
+const Sequelize = require("sequelize"); // const dbConfig = require("../config/database");
 
-const dbConfig = require("../config/database");
 
 const Customer = require("../models/Customer");
 
-const connection = new Sequelize(dbConfig);
+const connection = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: true
+  }
+});
 Customer.init(connection);
 module.exports = connection;
 //# sourceMappingURL=postgres.js.map
