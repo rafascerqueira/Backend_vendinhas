76dfda508f41ed3bfa428bd257874365434a7919
Rafael Cerqueira <rafascerqueira.dev@gmail.com>
2022-10-15 22:58:42 -0300

    chore: replace dist folder

--- dist/shared/infra/http/server.js
+++ dist/shared/infra/http/server.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = require("dotenv");
 const app_1 = require("./app");
-const port = 3001;
+(0, dotenv_1.config)();
+const port = process.env.PORT;
 app_1.app.listen(port, () => console.log(`Server is running at port: ${port}`));
 //# sourceMappingURL=server.js.map
